b61278075c94b56b5f4161037c801bd6b9bc74f3
Crew Spence <csspence123@gmail.com>
2020-05-31 05:21:31 -0500

    Wrote working solution.

--- same_frequency_udemy_toy_problem.js
+++ same_frequency_udemy_toy_problem.js
@@ -14,6 +14,28 @@ sameFrequency(22,222) // false
 */
 
 const sameFrequency = (num1, num2) => {
-  
+  let numStr1 = num1.toString();
+  let numStr2 = num2.toString();
+  let numObj = {};
+  if(numStr1.length !== numStr2.length) {
+    return false;
+  }
+  for(let i = 0; i < numStr1.length; i++) {
+    if(numObj[numStr1[i]] === undefined) {
+      numObj[numStr1[i]] = 1;
+    } else {
+      numObj[numStr1[i]]++
+    }
+  }
+  for(let i = 0; i < numStr2.length; i++) {
+    if(numObj[numStr2[i]] === undefined || numObj[numStr2[i]] === 0) {
+      return false;
+    }
+    if(numObj[numStr2[i]] > 1) {
+      numObj[numStr2[i]]--;
+    }
+  }
+
+  return true;
 }
 
