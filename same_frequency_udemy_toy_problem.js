c3488a59da1e9f5d130d0913bae59b44bb867ed7
Crew Spence <csspence123@gmail.com>
2020-05-31 05:14:11 -0500

    Created function stub for toy problem.

---
+++ same_frequency_udemy_toy_problem.js
@@ -0,0 +1,19 @@
+/*
+Udemy JavaScript Algorithms and Data Structures Masterclass
+
+Write a function called sameFrequency.  Given two positive integers, find
+out if the two numbers have the same frequency of digits.
+
+Your solution must have the following complexities:
+Time: O(N)
+Same Input:
+sameFrequency(182,281) // true
+sameFrequency(34,14) // false
+sameFrequency(3589578,5879385) // true
+sameFrequency(22,222) // false
+*/
+
+const sameFrequency = (num1, num2) => {
+  
+}
+
